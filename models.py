bbc5ae97d1eb36757f255363547dde7829c7d210
bran259 <kiprop686@gmail.com>
2026-01-05 10:33:06 +0300

    modified models

---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+#models
+
+class User(db.Model):
+    __tablename = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    email = db.Column(db.String, unique=True)
+
+class Post(db.Model):
+    __tablename = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title= db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    post_image = db.Column(db,String)
+
