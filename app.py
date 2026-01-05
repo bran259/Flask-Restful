908eb54e4fb0ec0b9f900db952939da3b086d556
bran259 <kiprop686@gmail.com>
2026-01-05 10:43:05 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from models import db, User, Post
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+#database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rest-api.db'
+
+migrate = Migrate(db, app)
+db.init_app(app)
 
 @app.route('/')
 def index():
